a4adc15120ca503fde88e5f5707ea68600a774cc
GalaxyShad <vadim.kadyrin@mail.ru>
2024-11-26 03:38:21 +0300

    [+] hope that this fix fixes the problem :D

--- bitonic_multi_cycle.sv
+++ bitonic_multi_cycle.sv
@@ -21,11 +21,12 @@ logic [3:0] stage = 0;
 
 assign sorted_list_o = sorted_list_reg;
 
-function void compare_and_swap(    
+function automatic void compare_and_swap(    
     inout logic [LIST_VALUE_BIT_COUNT-1:0] a,
     inout logic [LIST_VALUE_BIT_COUNT-1:0] b
 );
     logic[LIST_VALUE_BIT_COUNT-1:0] temp;    
+    
     if (a > b) begin
         temp = a;
         a = b;        
@@ -34,24 +35,26 @@ function void compare_and_swap(
 endfunction
 
 always @(posedge clk_i) begin    
-    if (rst_i)              stage <= 0;
-    else if (stage == 7)    stage <= 0;
-    else                    stage <= stage + 1;
+    if (rst_i) begin
+        stage <= 0;
+        buffer <= original_list_i;
+    end else if (stage == 5) begin 
+        stage <= 0;
+        buffer <= original_list_i;
+        sorted_list_reg <= buffer;
+    end else begin 
+        stage <= stage + 1;
+    end
 end
 
 always @* begin    
-    case (stage)
-        ///////////////////////
-        // Copy to buffer
-        ///////////////////////
-        0: buffer = original_list_i;
-            
+    case (stage)            
         ///////////////////////
         // STAGE 1
         ///////////////////////
 
         // STAGE 1
-        1: begin            
+        0: begin            
             compare_and_swap(buffer[0], buffer[1]); // # -> 
             compare_and_swap(buffer[3], buffer[2]); // # <-            
             compare_and_swap(buffer[4], buffer[5]); // # ->
@@ -63,7 +66,7 @@ always @* begin
         ///////////////////////
 
         // STAGE 2.1
-        2: begin            
+        1: begin            
             compare_and_swap(buffer[0], buffer[2]); // # ->
             compare_and_swap(buffer[1], buffer[3]); // # ->            
             compare_and_swap(buffer[6], buffer[4]); // # <-
@@ -71,7 +74,7 @@ always @* begin
         end
 
         // STAGE 2.2
-        3: begin
+        2: begin
             compare_and_swap(buffer[0], buffer[1]); // # ->            
             compare_and_swap(buffer[2], buffer[3]); // # ->
             compare_and_swap(buffer[5], buffer[4]); // # <-            
@@ -83,7 +86,7 @@ always @* begin
         ///////////////////////
 
         // STAGE 3.1        
-        4: begin
+        3: begin
             compare_and_swap(buffer[0], buffer[4]); // # ->            
             compare_and_swap(buffer[1], buffer[5]); // # ->
             compare_and_swap(buffer[2], buffer[6]); // # ->            
@@ -91,7 +94,7 @@ always @* begin
         end   
 
         // STAGE 3.2
-        5: begin            
+        4: begin            
             compare_and_swap(buffer[0], buffer[2]); // # ->
             compare_and_swap(buffer[1], buffer[3]); // # ->            
             compare_and_swap(buffer[4], buffer[6]); // # ->
@@ -99,19 +102,12 @@ always @* begin
         end
 
         // STAGE 3.3
-        6: begin
+        5: begin
             compare_and_swap(buffer[0], buffer[1]); // # ->            
             compare_and_swap(buffer[2], buffer[3]); // # ->
             compare_and_swap(buffer[4], buffer[5]); // # ->            
             compare_and_swap(buffer[6], buffer[7]); // # -> 
         end    
-
-        ///////////////////////
-        // Copy to ouput
-        ///////////////////////
-        7: begin
-            sorted_list_reg = buffer;
-        end
     endcase
 end 
 endmodule
